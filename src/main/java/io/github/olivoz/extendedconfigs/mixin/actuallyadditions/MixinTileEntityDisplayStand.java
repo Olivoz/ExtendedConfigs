f5c9b28e41b569c1f2d351582d932e970b52009c
Olivoz <olivoz12345@gmail.com>
2023-07-10 14:20:09 +0200

    Mixin fixes

--- src/main/java/io/github/olivoz/extendedconfigs/mixin/actuallyadditions/MixinTileEntityDisplayStand.java
+++ src/main/java/io/github/olivoz/extendedconfigs/mixin/actuallyadditions/MixinTileEntityDisplayStand.java
@@ -5,11 +5,13 @@ import de.ellpeck.actuallyadditions.mod.tile.TileEntityDisplayStand;
 import io.github.olivoz.extendedconfigs.configs.Config;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(value = TileEntityDisplayStand.class, remap = false)
 public abstract class MixinTileEntityDisplayStand {
 
+    @Mutable
     @Final
     @Shadow
     public CustomEnergyStorage storage = new CustomEnergyStorage(Config.ACTUALLY_ADDITIONS.displayStand.capacity, Config.ACTUALLY_ADDITIONS.displayStand.maxReceive, Config.ACTUALLY_ADDITIONS.displayStand.maxExtract);
